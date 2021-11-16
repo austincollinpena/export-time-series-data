2bc0ce8403cbe386246345e15ef5f99d911b1b0a
AustinPena <austinpena@taloflow.ai>
2021-11-16 09:17:00 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# export-time-series-data
+# Export Google Ads To Time Series Data
+
+Export any query defined by [Adwords Query Language](https://developers.google.com/adwords/api/docs/guides/awql) into time series data.
+
+[Here's a handy generator for Adwords Queries.](https://scriptninja.de/en/blog/awql)
+
+## How To Customize
+
+This script takes in **four** diferrent inputs.
+
+#### One: Write Your Query
+
+To make this as readable as possible, I seperated out each clause from the query into multiple lines.
+
+The ```during``` line is the date. Add your date ranges like so, with the older date in the past:
+
+```YYYYMMDD```,```YYYYMMDD```
+
+```js
+        {
+            "select": "SELECT CampaignName, Clicks, Ctr, AverageCpc, CampaignId, CostPerConversion, AllConversionValue, InvalidClicks, SearchClickShare, TopImpressionPercentage, AbsoluteTopImpressionPercentage, Date, Impressions, Cost",
+            "from": "FROM CAMPAIGN_PERFORMANCE_REPORT",
+            "where": "WHERE Impressions > 0",
+            "during": "DURING 20210501,20211107",
+        ...
+```
+
+#### Two: Your Target Google Sheet
+
+To get the sheet integration to work, you should update two variables: ```url``` and ```tabname```.
+
+```js
+            ...
+            "url": "enter-sheet-url-here",
+            "tabName": "enter-tabname-here",
+        },
+```
+
+#### Three: If You Should Clear The Sheet
+
+Big queries over long periods of time tend to run past the 30 minute timeout window.
+
+You can run the script multiple times over different time periods to get your data.
+
+Keep these two variables ```false``` in order to just keep writing data.
+
+```js
+var THE_SCRIPT_CLEAR_THE_CONTENTS_OF_THE_SHEET_CONTENTS = false;
+
+var ADD_COLUMN_HEADERS = false;
+```
+
+#### Four: Have an MCC?
+
+The ```export-data-account-view.js``` file is built for you. Be sure to add the ```ACCOUNT_IDS``` to this section. To have multiple, it looks like so:
+
+```js
+var ACCOUNT_IDS = ["123-456-7890", "543-210-9876"];
+
+```
+
+## Feature Request?
+
+Drop into the issues tab and let me know if you want the script improved or something added.
---
+++ export-data-account-view.js
@@ -0,0 +1,56 @@
+// Want the old cells on the sheet deleted? Change this to true
+var THE_SCRIPT_CLEAR_THE_CONTENTS_OF_THE_SHEET_CONTENTS = false;
+
+// Should the script add the column headers, or just the data?
+var ADD_COLUMN_HEADERS = true;
+
+
+function main() {
+    var QUERIES = [
+        {
+            "select": "SELECT CampaignName, Clicks, Ctr, AverageCpc, CampaignId, CostPerConversion, AllConversionValue, InvalidClicks, SearchClickShare, TopImpressionPercentage, AbsoluteTopImpressionPercentage, Date, Impressions, Cost",
+            "from": "FROM CAMPAIGN_PERFORMANCE_REPORT",
+            "where": "WHERE Impressions > 0",
+            "during": "DURING 20210501,20211107",
+            "url": "enter-sheet-url-here",
+            "tabName": "enter-tabname-here",
+        },
+        {
+            "select": "SELECT AllConversions, ConversionTypeName, CampaignName, CampaignId, Date",
+            "from": "FROM CAMPAIGN_PERFORMANCE_REPORT",
+            "where": "WHERE AllConversions > 0",
+            "during": "DURING 20210501,20211107",
+            "url": "enter-sheet-url-here",
+            "tabName": "enter-tabname-here",
+        }
+    ];
+
+    for (var i in QUERIES) {
+        var queryObject = QUERIES[i];
+        var query = queryObject.select + " " + queryObject.from + " " + queryObject.where + " " + queryObject.during;
+        var spreadsheet = SpreadsheetApp.openByUrl(queryObject.url);
+        var sheet = spreadsheet.getSheetByName(queryObject.tabName);
+        // If you don't want to clear
+        if (THE_SCRIPT_CLEAR_THE_CONTENTS_OF_THE_SHEET_CONTENTS) {
+            sheet.clear();
+        }
+        var report = AdWordsApp.report(query);
+        // This bit of code grabs the SELECT line and pulls out the variables,
+        // allowing you to ONLY write your variables on the select line and auto add them to the report in the right place
+        var columnNamesAsString = queryObject.select.replace("SELECT ", "");
+        var columnNamesAsArray = columnNamesAsString.split(", ");
+        if (ADD_COLUMN_HEADERS) {
+            sheet.appendRow(columnNamesAsArray);
+        }
+        var rows = report.rows();
+        while (rows.hasNext()) {
+            var currentRowArray = [];
+            var row = rows.next();
+            columnNamesAsArray.map(function (name) {
+                var currentColumn = row[name];
+                currentRowArray.push(currentColumn);
+            });
+            sheet.appendRow(currentRowArray);
+        }
+    }
+}
