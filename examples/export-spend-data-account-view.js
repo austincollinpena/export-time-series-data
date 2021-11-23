0aeed919b552f74b4a0df8a221f86b9d93f6d3e6
AustinPena <austinpena@taloflow.ai>
2021-11-23 09:06:41 -0500

    Removed extra lines to simplify example

--- examples/export-spend-data-account-view.js
+++ examples/export-spend-data-account-view.js
@@ -1,8 +1,4 @@
-// Want the old cells on the sheet deleted? Change this to true
-var THE_SCRIPT_CLEAR_THE_CONTENTS_OF_THE_SHEET_CONTENTS = false;
-
-// Should the script add the column headers, or just the data?
-var ADD_COLUMN_HEADERS = false;
+// Export account spend from a start date all the way to when the script is run.
 
 // This is the start date for the report in YYYYMMDD format
 var startDate = "20211101"
@@ -30,18 +26,11 @@ function main() {
         var query = queryObject.select + " " + queryObject.from + " " + queryObject.where + " " + queryObject.during;
         var spreadsheet = SpreadsheetApp.openByUrl(queryObject.url);
         var sheet = spreadsheet.getSheetByName(queryObject.tabName);
-        // If you don't want to clear
-        if (THE_SCRIPT_CLEAR_THE_CONTENTS_OF_THE_SHEET_CONTENTS) {
-            sheet.clear();
-        }
         var report = AdWordsApp.report(query);
         // This bit of code grabs the SELECT line and pulls out the variables,
         // allowing you to ONLY write your variables on the select line and auto add them to the report in the right place
         var columnNamesAsString = queryObject.select.replace("SELECT ", "");
         var columnNamesAsArray = columnNamesAsString.split(", ");
-        if (ADD_COLUMN_HEADERS) {
-            sheet.appendRow(columnNamesAsArray);
-        }
         report.exportToSheet(sheet);
     }
 }
